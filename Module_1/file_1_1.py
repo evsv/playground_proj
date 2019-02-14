98b9a7012d873907f8ab66f95773560027358273
evsv <vishwanathevs@outlook.com>
2019-02-13 20:35:32 -0600

    conflict function commit

--- Module_1/file_1_1.py
+++ Module_1/file_1_1.py
@@ -23,3 +23,20 @@ def hairness_do_stuff(h, y, c):
 
     return None
 
+def hairness_mcstern_conflict(d, b, z):
+    """
+    Oh noes! This function has been set up to cause a git conflict
+    between MC Stern and Her Hairness. Who will win? Who will prevail?
+    Find out next time on dragon.ball.z.
+    
+    :param d: [description]
+    :type d: [type]
+    :param b: [description]
+    :type b: [type]
+    :param z: [description]
+    :type z: [type]
+    :return: [description]
+    :rtype: [type]
+    """
+
+    return None
